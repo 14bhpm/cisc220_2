03731d9ee4f4b7477bbdbbdb870e49577d59017e
Beltan Michel <14bhpm@queensu.ca>
2016-10-31 23:45:58 -0400

    Tried fixing recursive function, worked to some extent but not completely.

--- processesTree.sh
+++ processesTree.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
+#Zina Al-Sabbagh 10161380
+#Rawan Alsaadi 10081442
+#Omar Toutounji 10169506
+#Beltan Michel 10169162
 
-# MAIN LIST WITH ALL INFORMATION
+#Recursion doesn't work (don't understand why) -> it doesn't keep executing the for loop when coming back to previous instance or recurse
+
 export var=()
 mapfile -t var < <(ps axo comm,pid,ppid,user,start | grep -e '')
-
-
-
 export incr=0
 export numProcesses=${#var[*]}
 export listPpid=()
 mapfile -t listPpid < <(ps axo comm,pid,ppid,user,start | grep -e '' | awk '{print $3}')
 
-
-
-
-
-#PRINTS INFO IN MAIN LIST
-
-
-
-# ALGORITHM SO FAR
-#1. Go through each pid and ppid in list
-#2. if pid == (ppid) then it is a child
-#3. if ppid == 1 then it is a child of systemd
-
 function recurse {
 ((incr=$incr+1))
 for (( i=1; i<numProcesses; i++ )); do
 	if [[ ${listPpid[i]} -eq $1 ]]; then
+		for ((n=1; n<incr; n++)); do
+			printf '\t'
+		done
 		echo ${var[i]}
-		echo var[i] | awk '{print $2}' && echo swag
-		echo var[i] | awk '{print $2}' | recurse
+		PID=$(echo ${var[i]} | awk '{print $2}')
+		recurse $PID
 	fi
 done 
 ((incr=$incr-1))
+return 0
 }
 
 echo $numProcesses
