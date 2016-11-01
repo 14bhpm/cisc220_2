a61a62f3fc8f13f931f5e5b80035fdd8bbf249ba
Zina Al-Sabbagh <z.alsabbagh@queensu.ca>
2016-10-31 20:04:18 -0400

    3rd commit!!!!!

--- firewallSetup.sh
+++ firewallSetup.sh
@@ -1,3 +1,10 @@
+#!bin/bin/bash
+
+# Rawan Alsaadi 10081442
+# Omar Toutounji 10169506
+# Beltan Michel 10169162
+# Zina Al-Sabbagh 10161380
+
 # delete any previous rules in the INPUT, FORWARD and OUTPUT tables
 sudo iptables -F INPUT
 sudo iptables -F FORWARD
@@ -7,10 +14,15 @@ sudo iptables -F OUTPUT
 sudo iptables -A INPUT -p TCP --dport 22 -s 130.15.0.0/16 -j ACCEPT
 
 # enable ssh from computers in any private network
+sudo iptables -A INPUT -p TCP --dport 22 -m iprange --src-range 10.0.0.0-10.255.255.255 -j ACCEPT
+sudo iptables -A INPUT -p TCP --dport 22 -m iprange --src-range 172.16.0.0-172.31.255.255 -j ACCEPT
+sudo iptables -A INPUT -p TCP --dport 22 -s 192.168.0.0/16 -j ACCEPT
 
 # enable http and https from any IP
+sudo iptables -A INPUT -p TCP --dport 80,443 -j ACCEPT
 
 # drop any other incoming tcp traffic
+sudo iptables -A INPUT -p TCP -j DROP
 
 #block connecting from your computer to any IP outside the range 130.15.0.0 to 130.15.255.255 [netmask/16]
 
